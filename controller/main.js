8b55f5b4dc33a47565d66db7e3edddd3c4b2f83b
Nathan Hill <nathan.hill@mfg.at>
2025-08-19 09:50:10 +0200

    adding friends

--- controller/main.js
+++ controller/main.js
@@ -17,5 +17,24 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex - 1].style.display = "block";
 }
+
+fetch('https://randomuser.me/api/?results=42')
+      .then(response => response.json())
+      .then(data => {
+        const users = data.results;
+        const grid = document.getElementById('userGrid');
+
+        users.forEach(user => {
+          const card = document.createElement('div');
+          card.className = 'card user-img';
+
+          card.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+          `;
+
+          grid.appendChild(card);
+        });
+      })
+      .catch(error => console.error('Error:', error));
